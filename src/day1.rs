bc778a29dec1e6c90782c8d9ee2cbf0ad171b8bc
Jade Guiton <guiton.jade@gmail.com>
2023-12-01 14:16:43 +0900

    day 1 - reorganize code

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let input = include_str!("../../inputs/day1.txt");
+    let input = include_str!("../inputs/day1.txt");
     
     let mut calibration_sum = 0;
     for line in input.lines() {
